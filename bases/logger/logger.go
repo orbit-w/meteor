1c2e9df50188a14e9f6e811fd775ebb463472951
orbit-w <wbwwxm@gmail.com>
2024-07-20 21:04:48 +0800

    add: 开发环境logger实例化

--- bases/logger/logger.go
+++ bases/logger/logger.go
@@ -20,6 +20,20 @@ func New(fileName string, lv zapcore.Level) *zap.Logger {
 	return zap.New(core)
 }
 
+// NewDevelopmentLogger 实例化开发环境日志
+func NewDevelopmentLogger() *zap.Logger {
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	cfg.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+
+	log, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	return log
+}
+
 // Stop flushing any buffered log entries
 // Applications should take care to call Sync before exiting
 func Stop(log *zap.Logger) {
