3a1a82ac80b4f1d60788ba55c07d6c1f7dc248bb
orbit-w <wbwwxm@gmail.com>
2024-09-27 19:07:27 +0800

    iter: packet pool test

--- modules/net/packet/packet_test.go
+++ modules/net/packet/packet_test.go
@@ -3,6 +3,7 @@ package packet
 import (
 	"fmt"
 	"github.com/orbit-w/meteor/bases/math"
+	"github.com/orbit-w/meteor/bases/net/bigendian_buf"
 	"testing"
 )
 
@@ -27,5 +28,29 @@ func Test_GenericFls(t *testing.T) {
 	n := 65536
 	m := math.PowerOf2(n)
 	fmt.Println(math.GenericFls(m))
+	fmt.Println(131072 << 4)
 
+}
+
+func TestBufPool_Put(t *testing.T) {
+	p := NewPool(maxSize)
+	bp := bigendian_buf.NewWithInitialSize(1000)
+	_ = p.Put(bp)
+	bp = bigendian_buf.NewWithInitialSize(1024)
+	_ = p.Put(bp)
+	bp = bigendian_buf.NewWithInitialSize(2)
+	_ = p.Put(bp)
+
+	bp = bigendian_buf.NewWithInitialSize(1)
+	_ = p.Put(bp)
+}
+
+func Test_Pool_Get(t *testing.T) {
+	p := NewPool(maxSize)
+	bp := p.Get(1)
+	bp = bigendian_buf.NewWithInitialSize(1000)
+	fmt.Println(bp.Cap())
+	_ = p.Put(bp)
+	bp2 := p.Get(1000)
+	fmt.Println(bp2.Cap())
 }
